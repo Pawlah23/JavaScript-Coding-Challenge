c081fc455312eb87e811e0b4eba9265283f662e1
Pawlah23 <120652580+Pawlah23@users.noreply.github.com>
2024-02-28 17:24:56 +0100

    Create Coding-Challenges2.js

---
+++ Coding-Challenges2.js
@@ -0,0 +1,12 @@
+// function lengthConverter(m) {
+//         // convert to m and return the equivalent m value
+//         // coversion rate is 1000 mm to 1 m
+//         const mm = m * 1000
+//         return mm;
+//     }
+    
+//     const mmValue = lengthConverter(1000);
+    
+//     console.log(mmValue);
+
+    // Meter to Millimeter	mm = m × 1000
