d2f02a0b4535afa6b618ece5675eda75ec647900
Pawlah23 <120652580+Pawlah23@users.noreply.github.com>
2024-02-28 19:05:08 +0100

    Create Coding-Challenges3.js

---
+++ Coding-Challenges3.js
@@ -0,0 +1,9 @@
+ 
+// for(let number = 0; number <=100; number = number+1) {
+//     if(number % 2 === 0) {
+//         console.log(number +' is an even number')  
+//     } else {
+//         // console.log(number +' is an odd number')
+//     }
+    
+// }
